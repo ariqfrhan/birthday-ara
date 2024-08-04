2e09d4800a9f5e757a1d65bacf959cc0b97b5214
ariqfrhan <muhammadariq3@gmail.com>
2024-08-04 21:04:18 +0700

    `fix` : delete dark mode

--- src/PaperContainer.tsx
+++ src/PaperContainer.tsx
@@ -4,32 +4,32 @@ import Paper from "./components/Paper";
 const PaperContainer: React.FC = () => {
   return (
     <div className={"container"}>
-      <Paper className={"image font-sans font-semibold text-center"}>
+      <Paper className={"image font-sans font-semibold text-center text-black"}>
         <img src="/ara7.jpeg" className="w-64" alt="Love" />
         <p>
           Mang boleh bucin didepan <br /> para maba?
         </p>
       </Paper>
 
-      <Paper className={"image font-sans font-semibold text-center"}>
+      <Paper className={"image font-sans font-semibold text-center text-black"}>
         <img src="/ara8.jpeg" className="w-64" alt="Cute" />
         <p>Gas gallery date lagi?</p>
       </Paper>
 
-      <Paper className={"image font-sans font-semibold text-center"}>
+      <Paper className={"image font-sans font-semibold text-center text-black"}>
         <p>How can be</p>
         <p>someone so cute ❤️</p>
         <img src="/ara5.jpeg" className="h-64" alt="Cute" />
       </Paper>
 
-      <Paper className={"image"}>
+      <Paper className={"image text-black"}>
         <p className="p1">My Favorite</p>
         <p className="p2">Person 😍</p>
         <img src="/ara10.jpeg" className="h-64" alt="Cute" />
       </Paper>
 
       <Paper>
-        <p className="p1">Geser pake mouse yaa</p>
+        <p className="p1 text-black">Geser pake mouse yaa</p>
       </Paper>
     </div>
   );
